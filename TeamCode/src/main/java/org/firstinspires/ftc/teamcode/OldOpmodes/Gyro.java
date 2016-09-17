627d349515db76f68f8b7fd8711ed3c0a17ac88a
DrewZ <zoghbya8828@parkwayschools.net>
2016-09-17 13:56:42 -0500

    Added old opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OldOpmodes/Gyro.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode.OldOpmodes;
+
+import android.os.*;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+
+/**
+ * Created by dzogh_000 on 4/11/2016.
+ * class to handle gyro calibration
+ */
+public class Gyro extends LinearOpMode {
+    GyroSensor gyroC;
+    GyroSensor gyroT;
+    double calStraight;
+    //variable to hold angle
+    static double angle = 0;
+
+    //method for calibration
+    public Gyro() {
+    }
+
+    public void runOpMode() {
+    }
+
+    public void calibrate() throws InterruptedException {
+        gyroC = hardwareMap.gyroSensor.get("gy");
+
+        double sumValues = 0;
+        //create array for holding sample values
+        double[] gyroValues;
+        gyroValues = new double[20];
+        //collect the sample values
+        //f defines how many samples sbhould be taken
+        int f = 20;
+        for (int i = 0; i < f; i++) {
+            sleep(20);
+            gyroValues[i] = gyroC.rawZ();
+            //sum the sample values together
+            sumValues += gyroValues[i];
+        }
+        calStraight = sumValues / f;
+    }
+
+    public double getCalStraight() {
+        return calStraight;
+    }
+
+    public static double getAngle(){
+        return angle;
+    }
+
+    Runnable gyroIntegrationRunnable = new Runnable() {
+        public void run() {
+            android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
+            while (opModeIsActive()) {
+                gyroT = hardwareMap.gyroSensor.get("gy");
+
+                angle += (gyroT.rawZ() / 100);
+                telemetry.addData("Angle: ", angle);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                }
+            }
+
+        }
+    };
+
+    public void integrateGyro() {
+        Thread gyroIntegrator = new Thread(gyroIntegrationRunnable);
+        gyroIntegrator.setDaemon(true);
+        gyroIntegrator.start();
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OldOpmodes/Tank.java
@@ -0,0 +1,90 @@
+package org.firstinspires.ftc.teamcode.OldOpmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+import com.qualcomm.robotcore.util.Range;
+
+public class Tank extends OpMode {
+    //drive motors
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+    DcMotor rightTank;
+    DcMotor leftTank;
+    Servo servo;
+    Servo flipper;
+    UltrasonicSensor front;
+    //Attempt to make sound
+    //drive values
+    double left;
+    double right;
+    double a = 1;
+    //scale for expo
+
+
+
+    @Override
+    public void init() {
+
+        //Mapping physical motors to variable
+        leftMotor = hardwareMap.dcMotor.get("left_Motor");
+        rightMotor = hardwareMap.dcMotor.get("right_Motor");
+        leftTank = hardwareMap.dcMotor.get("left_tank");
+        rightTank = hardwareMap.dcMotor.get("right_tank");
+        servo = hardwareMap.servo.get("servo");
+        flipper = hardwareMap.servo.get("flip");
+        front = hardwareMap.ultrasonicSensor.get("front");
+
+        leftMotor.setDirection(DcMotor.Direction.REVERSE );
+        rightTank.setDirection(DcMotor.Direction.REVERSE);
+        leftTank.setDirection(DcMotor.Direction.REVERSE);
+        rightMotor.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    @Override
+    public void loop() {
+        //getting drive values from controller
+        left = gamepad1.left_stick_y;
+        right = gamepad1.right_stick_y;
+        //clipping values so they dont exceed 100% on motors
+        left = Range.clip(left, -1, 1);
+        right = Range.clip(right, -1, 1);
+        //scale drive values for easier controlling
+        left = Drive.expo(left);
+        right = Drive.expo(right);
+
+        if (gamepad2.x)
+            servo.setPosition(1);
+        if (gamepad2.b)
+            servo.setPosition(0);
+        if (gamepad2.y)
+            servo.setPosition(0.5);
+        if (gamepad2.dpad_down)
+            a = 0.3;
+        if (gamepad2.dpad_up)
+            a = 1;
+        if (gamepad2.right_bumper)
+            flipper.setPosition(0.8);
+        if (gamepad2.left_bumper)
+            flipper.setPosition(0.2);
+        //set drive values for
+        left = left * a;
+        right = right * a;
+        leftMotor.setPower(left);
+        rightMotor.setPower(right);
+        leftTank.setPower(left);
+        rightTank.setPower(right);
+
+
+
+        //telemetry
+        telemetry.addData("Left Motor power", left);
+		telemetry.addData("Right Motor power", right);
+        telemetry.addData("Servo Position", servo.getPosition());
+        telemetry.addData("Distance in Front", front.getUltrasonicLevel());
+        telemetry.addData("Speed", a);
+    }
+
+
+}
