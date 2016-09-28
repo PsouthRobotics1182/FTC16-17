1272ba3c0920e2131af799a4a69e54e6fa12713f
psouthrobotics1182 <zoghbya8828@parkwayschools.net>
2016-09-28 14:53:02 -0500

    New teleop class for test robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBot.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.UltrasonicSensor;
@@ -8,23 +9,17 @@ import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.OldOpmodes.Drive;
 
+
+@TeleOp(name = "TestBot", group = "TeleOp")
 public class TestBot extends OpMode {
     //drive motors
     DcMotor leftMotor;
     DcMotor rightMotor;
-    DcMotor rightTank;
-    DcMotor leftTank;
-    Servo servo;
-    Servo flipper;
-    UltrasonicSensor front;
-    //Attempt to make sound
     //drive values
     double left;
     double right;
     double a = 1;
     //scale for expo
-
-
 
     @Override
     public void init() {
@@ -32,15 +27,8 @@ public class TestBot extends OpMode {
         //Mapping physical motors to variable
         leftMotor = hardwareMap.dcMotor.get("left_Motor");
         rightMotor = hardwareMap.dcMotor.get("right_Motor");
-        leftTank = hardwareMap.dcMotor.get("left_tank");
-        rightTank = hardwareMap.dcMotor.get("right_tank");
-        servo = hardwareMap.servo.get("servo");
-        flipper = hardwareMap.servo.get("flip");
-        front = hardwareMap.ultrasonicSensor.get("front");
 
-        leftMotor.setDirection(DcMotor.Direction.REVERSE );
-        rightTank.setDirection(DcMotor.Direction.REVERSE);
-        leftTank.setDirection(DcMotor.Direction.REVERSE);
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
         rightMotor.setDirection(DcMotor.Direction.FORWARD);
     }
 
@@ -56,35 +44,21 @@ public class TestBot extends OpMode {
         left = Drive.expo(left);
         right = Drive.expo(right);
 
-        if (gamepad2.x)
-            servo.setPosition(1);
-        if (gamepad2.b)
-            servo.setPosition(0);
-        if (gamepad2.y)
-            servo.setPosition(0.5);
+
         if (gamepad2.dpad_down)
             a = 0.3;
         if (gamepad2.dpad_up)
             a = 1;
-        if (gamepad2.right_bumper)
-            flipper.setPosition(0.8);
-        if (gamepad2.left_bumper)
-            flipper.setPosition(0.2);
+
         //set drive values for
         left = left * a;
         right = right * a;
         leftMotor.setPower(left);
         rightMotor.setPower(right);
-        leftTank.setPower(left);
-        rightTank.setPower(right);
-
-
 
         //telemetry
         telemetry.addData("Left Motor power", left);
 		telemetry.addData("Right Motor power", right);
-        telemetry.addData("Servo Position", servo.getPosition());
-        telemetry.addData("Distance in Front", front.getUltrasonicLevel());
         telemetry.addData("Speed", a);
     }
 
