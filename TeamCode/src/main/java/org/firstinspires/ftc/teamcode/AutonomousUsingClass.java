dba5c553cf5e2ef53aa3c823c26813c8e88f81f3
psouthrobotics1182 <zoghbya8828@parkwayschools.net>
2016-10-07 10:17:57 -0500

    fixing vuforia

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousUsingClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousUsingClass.java
@@ -3,12 +3,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 @Autonomous
 public class AutonomousUsingClass extends LinearOpMode {
-    DriveSystem wheels = new DriveSystem();
+    DriveSystem wheels;
 
     @Override
     public void runOpMode() throws InterruptedException{
+        wheels = new DriveSystem();
         wheels.configureMotors(2, DcMotorSimple.Direction.FORWARD, DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
